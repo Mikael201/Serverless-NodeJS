9dac8f5c8393837e8c3b8ed81a8f027e82f48d23
Mikael <mikael.heininen@student.uwasa.fi>
2020-05-15 14:02:50 +0300

    added res

--- GetNames/index.js
+++ GetNames/index.js
@@ -3,21 +3,17 @@ const mongoose = require('mongoose')
 const scoreSchema = require('../scores')
 const url = process.env["URL"]
 const mongooseConnector = mongoose.connect(url, {useNewUrlParser:true})
-module.exports = (event, context) => {
+module.exports = async (context) => {
     context.callbackWaitForEmptyEventLoop = false;
     mongooseConnector.then(() => {
         scoreSchema.find({
 
         }).then(scores => {
             console.log(scores)
+            context.res = {
+                status: 200, //200 on defaultti
+                body: scores
+            }
         })
     });
 }
-
-
-/*
-userModel.findOne({
-
-}).then(user => {
-
-})*/
